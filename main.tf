b4cf61bda91fd0333a0281b86c886accb0c3ac6b
naveenkumarnk591504 <138796140+naveenkumarnk591504@users.noreply.github.com>
2023-08-14 11:58:46 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,12 +1,12 @@
 module "ec2_instance" {
   source  = "terraform-aws-modules/ec2-instance/aws"
 
-  name = "jenkins-instance"
+  name = "nexus-tool"
   ami  = "ami-0da59f1af71ea4ad2"
   instance_type          = "t2.medium"
   key_name               = "naveen"
   monitoring             = true
   vpc_security_group_ids = ["sg-05c859b8a7b7625fe"]
   subnet_id              = "subnet-018e7103a3134f584"
-  user_data = file("jenkins.sh")
+  user_data = file("nexus.sh")
 }
